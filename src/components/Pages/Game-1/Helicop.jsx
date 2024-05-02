35856aa9bf7ae60979558d41ed7667afd98ae5b9
enrfercas <114150760+enrfercas@users.noreply.github.com>
2024-05-02 12:53:44 +0200

    El juego apareciendo donde debe y efecto de auto-rellenado a la descripción.

--- src/components/Pages/Game-1/Helicop.jsx
+++ src/components/Pages/Game-1/Helicop.jsx
@@ -2,129 +2,126 @@ import { useState } from "react";
 import "./Helicop.css";
 
 export const Helicop = () => {
-  // Estado para controlar si se muestra el iframe
   const [mostrarIframe, setMostrarIframe] = useState(false);
 
-  // Función para mostrar el iframe cuando se hace clic en el botón "Jugar"
   const mostrarIframeHandler = () => {
-    setMostrarIframe(!mostrarIframe);
+    console.log("Cambiando el estado de mostrarIframe");
+    setMostrarIframe(true);
   };
-  // Función para cerrar el iframe desde dentro de la modal
+
   const cerrarModalHandler = () => {
     setMostrarIframe(false);
   };
 
   return (
     <>
-    {/* Modal */}
-    {mostrarIframe && (
-          <>          
+      <div className="header-spacer"></div>
+      {mostrarIframe && (
+        <>
           <div className="header-spacer"></div>
-          <div className="modal-dialog modal-fullscreen mt-5">
-            <div className="modal-content">
-              <div className="modal-header">
+          <div
+            className="modal-dialog modal-fullscreen-lg-down"
+            style={{ zIndex: 2000 }}
+          >
+            <div className="modal-content d-flex w-100">
+              <div className="modal-header mx-auto">
                 <h5 className="modal-title">Helicopter</h5>
                 <button
                   type="button"
-                  className="btn-close"
+                  className="btn-close ms-3"
                   onClick={cerrarModalHandler}
                   aria-label="Close"
                 ></button>
               </div>
               <div className="modal-body">
-                {/* Aquí va el iframe */}
                 <iframe
                   className="juego"
                   scrolling="no"
-                  allowFullScreen="si"
+                  allowFullScreen={true}
                   frameBorder="0"
                   src="https://html-classic.itch.zone/html/8561483/index.html"
-                  style={{ width: 1500 }}
                 ></iframe>
               </div>
-              {/* <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={cerrarModalHandler}>Close</button>
-              </div> */}
             </div>
           </div>
-          </>
-        )}
-      <div className="container-fluid">
-        <div className="row mt-5">
-          <div className="col-12 d-flex justify-content-center align-items-center">
-            
-            <div
-              id="carouselExampleAutoplaying"
-              className="carousel slide mt-5"
-              data-bs-ride="carousel"
-              style={{ width: "50%" }}
-            >
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero0.png"
-                    className="d-block w-100"
-                  />
+        </>
+      )}
+      {!mostrarIframe && (
+        <div className="container-fluid">
+          <div className="row mt-5">
+            <div className="col-12 d-flex justify-content-center align-items-center">
+              <div
+                id="carouselExampleAutoplaying"
+                className="carousel slide mt-5"
+                data-bs-ride="carousel"
+                style={{ width: "50%", zIndex: 1 }}
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero0.png"
+                      className="d-block w-100"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero1.png"
+                      className="d-block w-100"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero2.png"
+                      className="d-block w-100"
+                    />
+                  </div>
                 </div>
-                <div className="carousel-item">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero1.png"
-                    className="d-block w-100"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Helicoptero/Helicoptero2.png"
-                    className="d-block w-100"
-                  />
-                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleAutoplaying"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleAutoplaying"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
               </div>
-              <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col-lg-6">
+              <span className="description">
+                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                magni nam officiis soluta cum ipsam porro laudantium
+                consectetur, repellendus vel esse alias hic quibusdam eos minima
+                incidunt velit aut molestiae.
+              </span>
+            </div>
+            <div className="col-lg-6 mb-5">
+              <button className="game-button" onClick={mostrarIframeHandler}>
+                {" "}
+                Jugar{" "}
               </button>
             </div>
           </div>
         </div>
-        <div className="row mb-5">
-          <div className="col-lg-6">
-            <p className="text-white mt-2">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              magni nam officiis soluta cum ipsam porro laudantium consectetur,
-              repellendus vel esse alias hic quibusdam eos minima incidunt velit
-              aut molestiae.{" "}
-            </p>
-          </div>
-          <div className="col-lg-6 mb-5">
-            <button className="game-button" onClick={mostrarIframeHandler}>
-              {" "}
-              Jugar{" "}
-            </button>
-          </div>
-        </div>
-        
-      </div>
+      )}
     </>
   );
 };
