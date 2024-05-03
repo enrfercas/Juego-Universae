024711b4fabe58d6f150f48ac222b9f93094202b
fatisand <fatiisandoval2000@gmail.com>
2024-05-03 14:36:57 +0200

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from './components/Header.jsx';
 import { Routes, Route } from 'react-router';
 import { Home } from './components/Pages/Home/Home.jsx'
 import { Footer } from './components/Footer.jsx'
+import { Robot } from './components/Pages/Game-4/Robot.jsx';
 
 function App() {
   
@@ -10,7 +11,8 @@ function App() {
     <>      
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>        
+        <Route path='/' element={<Home></Home>}></Route>  
+        <Route path='/Robot' element={<Robot></Robot>}></Route>              
       </Routes>
       <Footer></Footer>
     </>
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Home.css';
 export const Home = () => {
   return (
@@ -74,9 +75,11 @@ export const Home = () => {
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco.
                 </p>
+                <Link to ='/Robot'>
                 <button className="game-button">
                   <span>Jugar</span>
                 </button>
+                </Link>
               </div>
             </div>
           </div>
