abb63c90673b09681a235b159d02c813fe63d8f9
Fatimadaw <166696603+Fatimadaw@users.noreply.github.com>
2024-05-07 12:42:45 +0200

    Modificacion juego 3

--- src/components/Pages/Game-3/Fire.jsx
+++ src/components/Pages/Game-3/Fire.jsx
@@ -1,156 +1,201 @@
 
 import "./Fire.css";
-import { useState } from "react";
+import "../Game-3/Fire.css";
+
+import { useState, useRef, useEffect } from "react";
+
 export const Fire = () => {
-  const [isModalOpen, setIsModalOpen]  = useState(false);
-  const abrirIframe = () => {
-    setIsModalOpen(true);
-    console.log ("Estoy seteando la modal: ",isModalOpen);
+  const [modalImageSrc, setModalImageSrc] = useState("");
+  const modalRef = useRef(null);
+  const iframeRef = useRef(null);
+
+  const handleImageClick = (src) => {
+    setModalImageSrc(src);
+    document.body.style.overflow = "hidden";
   };
-  const cerrarIframe = () => {
-    setIsModalOpen(false);
+
+  const handleClickOutsideImage = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setModalImageSrc("");
+      document.body.style.overflow = "auto";
+    }
   };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsideImage);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideImage);
+    };
+  }, []);
+
+  const [mostrarIframe, setMostrarIframe] = useState(false);
+
+  const mostrarIframeHandler = () => {
+    console.log("Cambiando el estado de mostrarIframe");
+    setMostrarIframe(true);
+  };
+
+  const cerrarModalHandler = () => {
+    setMostrarIframe(false);
+  };
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      if (iframeRef.current && mostrarIframe) {
+        iframeRef.current.style.width = "100%";
+        console.log('handle orientation: ', iframeRef);
+        location.reload();
+      }
+    };
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, [mostrarIframe]);
 
   return (
     <>
       <div className="header-spacer"></div>
-      {isModalOpen && (
+      {mostrarIframe && (
         <>
-          <div className="header-spacer"></div>
-          <div
-            className="modal-dialog modal-fullscreen-lg-down"
-            style={{ zIndex: 2000 }}
-          >
-            <div className="modal-content d-flex w-100">
-              <div className="card text-bg-light p-1">
-                <div className="modal-header mx-auto">
-                  <h5 className="modal-title mb-2">Fire</h5>
-                  <button className="button-game"
-                    type="button"
-                    onClick={cerrarIframe}
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="modal-body w-100">
-                  <iframe
-                    className="juego"
-                    scrolling="no"
-                    allowFullScreen={true}
-                    frameBorder="0"
-                    src="https://html-classic.itch.zone/html/8561483/index.html"
-                ></iframe>
+          <div className="container-fluid d-flex justify-content-center">
+            <div
+              className="modal-dialog modal-fullscreen-lg-centered"
+              style={{ zIndex: 2000, width: "80%" }}
+            >
+              <div className="modal-content d-flex w-100">
+                <div className="fondo-iframe">
+                  <div className="boton">
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={cerrarModalHandler}
+                      data-bs-theme="light"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+
+                  <div className="modal-body d-flex justify-content-center w-90">
+                    <iframe
+                      className="juego"
+                      scrolling="no"
+                      allowFullScreen={true}
+                      frameBorder="0"
+                      src="https://html-classic.itch.zone/html/8561483/index.html"
+                    ></iframe>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </>
       )}
-      {!isModalOpen && (
-      <div className="container-fluid">
-        <div className="row mt-5">
-          <div className="col-lg-12 d-flex justify-content-left align-items-left col-md-6 col-sm-6">
-            <div
-              id="carouselExampleAutoplaying"
-              className="carousel slide mt-5"
-              data-bs-ride="carousel"
-              style={{ width: "50%", zIndex: 1 }}
-            >
-              <div className="carousel-inner col-lg-6">
-                <div className="carousel-item active">
-                  <img
-                    src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion0.png"
-                    className="d-block w-100"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion1.png"
-                    className="d-block w-100"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion2.png"
-                    className="d-block w-100"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion3.png"
-                    className="d-block w-100"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    src="/public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion4.png"
-                    className="d-block w-100"
-                  />
-                </div>
-              </div>
-              <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleAutoplaying"
-                data-bs-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="visually-hidden">Next</span>
-              </button>
+      {!mostrarIframe && (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <h1 className="titulo-juego">
+                PROTOCOLO SOBRE EXTINCIÓN DE INCENDIOS
+              </h1>
             </div>
-            
-            <div className="card-3 ">
-              <div className="card__content-game3">
-                <div className="card__date-game3"> GAME DESCRIPTION</div>
-                <div className="card__info-game3">
-                  <h3>
-                    Bienvenido al juego de Extinción de incendios.<br></br>
-                  </h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
-                  </p>
-                </div>
-                <div className="col-lg-6 mb-5">
-                  <button className="button-game" onClick={abrirIframe}>
-                    COMENZAR
-                    <div id="clip">
-                      <div id="leftTop" className="corner"></div>
-                      <div id="rightBottom" className="corner"></div>
-                      <div id="rightTop" className="corner"></div>
-                      <div id="leftBottom" className="corner"></div>
-                    </div>
-                    <span id="rightArrow" className="arrow"></span>
-                    <span id="leftArrow" className="arrow"></span>
-                  </button>
-                </div>
-                
+          </div>
+          <div className="row">
+            <div className="col-lg-3">
+              <div className="juego-descripcion">
+                <p>
+                  Bienvenido al juego de extinción de incendios. Deberás familiarizarte con los
+                  controles y hacer esto:
+                </p>
+                <h2>Controles e instrucciones</h2>
+                <p>
+                  Puede comenzar la partida dando a <span>comenzar</span>, si lo
+                  prefiere puede hacer agrandar el juego haciendo{" "}
+                  <span>doble click</span> en el juego. Los controles seran{" "}
+                  <span>W</span> para avanzar <span>A</span> y <span>D</span>{" "}
+                  para desplazamiento lateral y <span>S</span> para ir hacia
+                  atrás. Los movimientos de cabeza se harán con el{" "}
+                  <span>Mouse</span>, y puede interactuar con los objetos de su
+                  entorno con la tecla <span>F</span>. Buena suerte con su
+                  partida.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-6 d-flex justify-content-center">
+              <img
+                className="imagen-boton"
+                src="/public/iframe-imagen.png"
+                onClick={mostrarIframeHandler}
+              />
+            </div>
+            <div className="col-lg-3 d-flex justify-content-center">
+              <div className="galeria-juego">
+                <h3>Galería</h3>
+                <img
+                  className="imagen-juego"
+                  onClick={() =>
+                    handleImageClick(
+                      "public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion0.png"
+                    )
+                  }
+                  src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion0.png"
+                  alt="imagen de juego"
+                />
+                <img
+                  className="imagen-juego"
+                  onClick={() =>
+                    handleImageClick(
+                      "public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion1.png"
+                    )
+                  }
+                  src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion1.png"
+                  alt="imagen de juego"
+                />
+                <img
+                  className="imagen-juego"
+                  onClick={() =>
+                    handleImageClick(
+                      "public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion2.png"
+                    )
+                  }
+                  src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion2.png"
+                  alt="imagen de juego"
+                />
+                <img
+                  className="imagen-juego"
+                  onClick={() =>
+                    handleImageClick(
+                      "public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion3.png"
+                    )
+                  }
+                  src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion3.png"
+                  alt="imagen de juego"
+                />
+                <img
+                  className="imagen-juego"
+                  onClick={() =>
+                    handleImageClick(
+                      "public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion4.png"
+                    )
+                  }
+                  src="public/Games-Images/InterfazGame/Miniaturas/Extincion/Extincion4.png"
+                  alt="imagen de juego"
+                />
+              </div>
+            </div>
+            <div className={`modal ${modalImageSrc ? "" : "modal-off"}`}>
+              <div ref={modalRef} className="modal-content">
+                <img
+                  className="imagen-modal"
+                  src={modalImageSrc}
+                  alt="imagen-modal"
+                />
               </div>
             </div>
           </div>
         </div>
-      </div>
-    )} 
-  </>
+      )}
+    </>
   );
 };
